03dffcd93439ae824e201789e98758c31eeef0ec
Ayush Raj <rajayush3182003@gmail.com>
2025-07-11 15:06:00 +0530

    Eigth Commit

---
+++ 8-Type_Guards/type_guards.ts
@@ -0,0 +1,15 @@
+function abcd(arg: string | number){
+    if(typeof arg === "number"){
+        return "number"
+    }
+    else if(typeof arg === "string"){
+        return "string"
+    }
+    else{
+        throw new Error("pagal hogaya hai kay friends");
+    }
+}
+
+console.log(abcd(12));
+console.log(abcd("hello"));
+//abcd(true); //this will give error
